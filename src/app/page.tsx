a19c1ada86f5db1d4ede48ab88bcfba859986dc4
Luiz Henrique <7henrique18@gmail.com>
2025-02-24 23:15:20 -0300

    refactor: update typography and layout for personal portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,26 +35,26 @@ export default function Home() {
 		<main className="min-h-screen py-12 px-4 flex justify-center">
 			<div className="max-w-md w-full space-y-16 mx-auto">
 				<section id="home">
-					<h1 className="font-semibold">Luiz Henrique</h1>
-					<h2 className="text-sm text-neutral-400">Software Engineer</h2>
+					<h1 className="text-lg font-semibold">Luiz Henrique</h1>
+					<h2 className="text-neutral-400">Software Engineer</h2>
 				</section>
 
 				<section id="about">
-					<h2 className="font-semibold">About</h2>
-					<p className="text-sm text-neutral-400">
+					<h2 className="text-lg font-semibold">About</h2>
+					<p className="text-neutral-400">
 						Software Engineer with 4+ years of experience, passionate about
 						building products and continuous learning.
 					</p>
 				</section>
 
 				<section id="work">
-					<h2 className="font-semibold">Work Experience</h2>
+					<h2 className="text-lg font-semibold">Work Experience</h2>
 
 					<div className="space-y-4 mt-2">
 						{WORK_EXPERIENCE.map((work) => (
 							<div className="flex justify-between" key={work.company}>
 								<div className="flex items-start gap-4">
-									<div className="size-8 bg-neutral-800 relative rounded-sm overflow-hidden border border-neutral-700">
+									<div className="size-10 bg-neutral-800 relative rounded-sm overflow-hidden border border-neutral-700">
 										<Image
 											src={work.icon}
 											alt={work.company}
@@ -63,12 +63,12 @@ export default function Home() {
 										/>
 									</div>
 
-									<div className="space-y-1">
+									<div className="">
 										<div className="flex items-center gap-2">
-											<h3 className="text-sm font-medium">{work.company}</h3>
+											<h3 className="text font-medium">{work.company}</h3>
 										</div>
 
-										<p className="text-xs text-neutral-400">{work.role}</p>
+										<p className="text-sm text-neutral-400">{work.role}</p>
 									</div>
 								</div>
 
